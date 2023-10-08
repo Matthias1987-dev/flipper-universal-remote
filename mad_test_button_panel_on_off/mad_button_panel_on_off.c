b70282acd82aa832ec035a3e9251b503ce514c55
Matthias1987-dev <116465576+Matthias1987-dev@users.noreply.github.com>
2023-10-08 11:06:50 +0200

    20231008
    Update

--- mad_test_button_panel_on_off/mad_button_panel_on_off.c
+++ mad_test_button_panel_on_off/mad_button_panel_on_off.c
@@ -3,37 +3,9 @@
 
 #include <gui/gui.h>
 #include <input/input.h>
-
 #include "mad_test_button_panel_on_off_icons.h"
 
-typedef enum {
-    MyEventTypeKey,
-    MyEventTypeDone,
-} MyEventType;
 
-typedef struct {
-    MyEventType type; // The reason for this event.
-    InputEvent input; // This data is specific to keypress data.
-} MyEvent;
-
-FuriMessageQueue* queue;
-int x = 32;
-int y = 48;
-
-static void my_input_callback(InputEvent* input_event, void* context) {
-    UNUSED(context);
-    if(input_event->type == InputTypeShort) {
-        if(input_event->key == InputKeyLeft) x--;
-        if(input_event->key == InputKeyRight) x++;
-        if(input_event->key == InputKeyUp) y--;
-        if(input_event->key == InputKeyDown) y++;
-        if(input_event->key == InputKeyBack) {
-            MyEvent event;
-            event.type = MyEventTypeDone;
-            furi_message_queue_put(queue, &event, FuriWaitForever);
-        }
-    }
-}
 
 void my_draw_background(Canvas* canvas, void* context) {
     UNUSED(context);
@@ -59,37 +31,37 @@ void my_draw_enter_2(Canvas* canvas, void* context) {
     canvas_draw_icon(canvas, 12, 35, &I_On_hvr_25x27);
 }
 
-static void my_draw_enter_3(Canvas* canvas, void* context) {
+void my_draw_enter_3(Canvas* canvas, void* context) {
     UNUSED(context);
     my_draw_background(canvas, context);
     canvas_draw_icon(canvas, 41, 5, &I_Off_hvr_25x27);
 }
 
-static void my_draw_enter_4(Canvas* canvas, void* context) {
+void my_draw_enter_4(Canvas* canvas, void* context) {
     UNUSED(context);
     my_draw_background(canvas, context);
     canvas_draw_icon(canvas, 41, 35, &I_On_hvr_25x27);
 }
 
-static void my_draw_enter_5(Canvas* canvas, void* context) {
+void my_draw_enter_5(Canvas* canvas, void* context) {
     UNUSED(context);
     my_draw_background(canvas, context);
     canvas_draw_icon(canvas, 70, 5, &I_Off_hvr_25x27);
 }
 
-static void my_draw_enter_6(Canvas* canvas, void* context) {
+void my_draw_enter_6(Canvas* canvas, void* context) {
     UNUSED(context);
     my_draw_background(canvas, context);
     canvas_draw_icon(canvas, 70, 35, &I_On_hvr_25x27);
 }
 
-static void my_draw_enter_7(Canvas* canvas, void* context) {
+void my_draw_enter_7(Canvas* canvas, void* context) {
     UNUSED(context);
     my_draw_background(canvas, context);
     canvas_draw_icon(canvas, 99, 5, &I_Off_hvr_25x27);
 }
 
-static void my_draw_enter_8(Canvas* canvas, void* context) {
+void my_draw_enter_8(Canvas* canvas, void* context) {
     UNUSED(context);
     my_draw_background(canvas, context);
     canvas_draw_icon(canvas, 99, 35, &I_On_hvr_25x27);
@@ -104,44 +76,24 @@ int mad_test_button_panel_on_off_app(void* p) {
     view_port_draw_callback_set(view_port, my_draw_background, NULL);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_background, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_1, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_2, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_3, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_4, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_5, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_6, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_7, NULL);
-    //
-    //    furi_delay_ms(1000);
-    //
-    //    view_port_draw_callback_set(view_port, my_draw_enter_8, NULL);
-    //
-    //    furi_delay_ms(5000);
-    //
+
+
+int pos = 1;
+do {
+if (pos == 1){view_port_draw_callback_set(view_port, my_draw_enter_1, NULL);}
+if (pos == 2){view_port_draw_callback_set(view_port, my_draw_enter_2, NULL);}
+if (pos == 3){view_port_draw_callback_set(view_port, my_draw_enter_3, NULL);}
+if (pos == 4){view_port_draw_callback_set(view_port, my_draw_enter_4, NULL);}
+if (pos == 5){view_port_draw_callback_set(view_port, my_draw_enter_5, NULL);}
+if (pos == 6){view_port_draw_callback_set(view_port, my_draw_enter_6, NULL);}
+if (pos == 7){view_port_draw_callback_set(view_port, my_draw_enter_7, NULL);}
+if (pos == 8){view_port_draw_callback_set(view_port, my_draw_enter_8, NULL);}
+
+        furi_delay_ms(1000);
+        pos++;
+while (pos == 8);
+}
+
     gui_remove_view_port(gui, view_port);
     return 0;
 }
