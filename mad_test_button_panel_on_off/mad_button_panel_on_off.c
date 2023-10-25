c0b5b58ee254a57b3bc533e7a293a30cb2bbbb24
Matthias1987-dev <116465576+Matthias1987-dev@users.noreply.github.com>
2023-10-25 21:17:00 +0200

    Copy Code from application_user
    copy the last state from application_user on 20231025

--- mad_test_button_panel_on_off/mad_button_panel_on_off.c
+++ mad_test_button_panel_on_off/mad_button_panel_on_off.c
@@ -2,10 +2,16 @@
 #include <furi_hal.h>
 
 #include <gui/gui.h>
+#include <gui/view.h>
 #include <input/input.h>
 #include "mad_test_button_panel_on_off_icons.h"
 
+//test vom hp laptop zu syncronisieren
+typedef struct {
+    uint8_t x, y;
+} ImagePosition;
 
+static ImagePosition image_position = {.x = 1, .y = 1};
 
 void my_draw_background(Canvas* canvas, void* context) {
     UNUSED(context);
@@ -67,8 +73,9 @@ void my_draw_enter_8(Canvas* canvas, void* context) {
     canvas_draw_icon(canvas, 99, 35, &I_On_hvr_25x27);
 }
 
-int mad_test_button_panel_on_off_app(void* p) {
+int32_t mad_test_button_panel_on_off_app(void* p) {
     UNUSED(p);
+    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(InputEvent));
 
     //Show directions to user.
     Gui* gui = furi_record_open(RECORD_GUI);
@@ -76,24 +83,58 @@ int mad_test_button_panel_on_off_app(void* p) {
     view_port_draw_callback_set(view_port, my_draw_background, NULL);
     gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
+    InputEvent event;
 
-
-int pos = 1;
-do {
-if (pos == 1){view_port_draw_callback_set(view_port, my_draw_enter_1, NULL);}
-if (pos == 2){view_port_draw_callback_set(view_port, my_draw_enter_2, NULL);}
-if (pos == 3){view_port_draw_callback_set(view_port, my_draw_enter_3, NULL);}
-if (pos == 4){view_port_draw_callback_set(view_port, my_draw_enter_4, NULL);}
-if (pos == 5){view_port_draw_callback_set(view_port, my_draw_enter_5, NULL);}
-if (pos == 6){view_port_draw_callback_set(view_port, my_draw_enter_6, NULL);}
-if (pos == 7){view_port_draw_callback_set(view_port, my_draw_enter_7, NULL);}
-if (pos == 8){view_port_draw_callback_set(view_port, my_draw_enter_8, NULL);}
-
-        furi_delay_ms(1000);
-        pos++;
-while (pos == 8);
-}
-
+    // bool running = true;
+    int running = 1;
+    while(running) {
+        if(furi_message_queue_get(event_queue, &event, 100) == FuriStatusOk) {
+            if((event.type == InputTypePress) || (event.type == InputTypeRepeat)) {
+                if(event.type == InputTypePress) {
+                    if(event.key == InputKeyUp) {
+                        image_position.y--;
+                        if(image_position.y < 1) {
+                            image_position.y = 1;
+                        }
+                    } else if(event.key == InputKeyDown) {
+                        image_position.y++;
+                        if(image_position.y > 2) {
+                            image_position.y = 2;
+                        }
+                    } else if(event.key == InputKeyLeft) {
+                        image_position.x--;
+                        if(image_position.x < 1) {
+                            image_position.x = 1;
+                        }
+                    } else if(event.key == InputKeyRight) {
+                        image_position.x++;
+                        if(image_position.x > 4) {
+                            image_position.x = 4;
+                        }
+                    }
+                    // Set the view port draw callback based on the new image position
+                    if(image_position.y == 1 && image_position.x == 1) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_1, NULL);
+                    } else if(image_position.y == 2 && image_position.x == 1) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_2, NULL);
+                    } else if(image_position.y == 1 && image_position.x == 2) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_3, NULL);
+                    } else if(image_position.y == 2 && image_position.x == 2) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_4, NULL);
+                    } else if(image_position.y == 1 && image_position.x == 3) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_5, NULL);
+                    } else if(image_position.y == 2 && image_position.x == 3) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_6, NULL);
+                    } else if(image_position.y == 1 && image_position.x == 4) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_7, NULL);
+                    } else if(image_position.y == 2 && image_position.x == 4) {
+                        view_port_draw_callback_set(view_port, my_draw_enter_8, NULL);
+                    }
+                }
+            }
+        }
+    }
     gui_remove_view_port(gui, view_port);
+
     return 0;
 }
